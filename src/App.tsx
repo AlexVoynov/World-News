aacd1c7e4e36efd6ca8d241993fa6b7543adae56
Kacper Żarna <kacper@192.168.1.11>
2023-01-11 20:50:26 +0100

    Kod 11.01.2023 20:50

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 function App() {
+
+  // console.log(avatarClicked)
   return (
     <div className="App">
       <Navbar />
@@ -9,3 +11,8 @@ function App() {
 }
 
 export default App;
+
+// 1. Stwórz stan avatarClicked w App.tsx. Wartość początkowa: false, typ useState: boolean.
+// 2. Przekaż zmienną stanową i funkcję aktualizującą stan do komponentu Navbar.
+// 3. W komponencie Navbar stwórz odpowiedni interface i odbierz propsy.
+// 4. Przy kliknięciu na avatar: jeżeli stan avatarClicked jest ustawiony na false to zmień na true. Jeżeli jest ustawiony na true to zmień na false. Stwórz do tego odpowiednią funkcję i przypisz ją do onClicka na avatarze.

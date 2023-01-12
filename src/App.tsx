7b906c828f5f88e0018cbf863d00daf1afe160de
Kacper Żarna <kacper@192.168.1.11>
2023-01-12 18:22:57 +0100

    Kod 12.01.2023 18:20

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Navbar
-        avatarClicked={avatarClicked}
         setAvatarClicked={setAvatarClicked}
       />
     </div>
@@ -14,5 +13,3 @@ function App() {
 }
 
 export default App;
-
-// 4. Przy kliknięciu na avatar: jeżeli stan avatarClicked jest ustawiony na false to zmień na true. Jeżeli jest ustawiony na true to zmień na false. Stwórz do tego odpowiednią funkcję i przypisz ją do onClicka na avatarze.

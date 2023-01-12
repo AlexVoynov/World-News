705e0912f155fade54d81c277468b47bfd5bcc90
Kacper Żarna <kacper@192.168.1.11>
2023-01-12 20:02:28 +0100

    Kod 12.01.2023 20:00

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./components/HomePage/HomePage";
 function App() {
   const [avatarClicked, setAvatarClicked] = useState<boolean>(false);
   console.log(avatarClicked);
   return (
     <div className="App">
-      <Navbar
-        setAvatarClicked={setAvatarClicked}
-      />
+      <Navbar setAvatarClicked={setAvatarClicked} />
+      <HomePage />
     </div>
   );
 }

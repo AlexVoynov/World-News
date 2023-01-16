26eba0329cfd1b82276078c11fe57d5205a53d65
Kacper Żarna <kacper@192.168.1.11>
2023-01-16 17:52:29 +0100

    Kod 12.01.2023 21:30

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { API_KEY } from "../../helpers/api";
+import { Typography, List } from "@mui/material";
 const HomePage = () => {
   const [todaysArticles, setTodaysArticles] = useState([]);
+
+  let toSieZmienia = 0;
 
   // 1. useEffect to hook służący do podpinania się pod konkretny moment cyklu życia komponentu.
   // 2. Cykl życia komponentu:
@@ -19,21 +22,32 @@ const HomePage = () => {
 
   const today = new Date();
   const day = today.getDate();
-  const month = today.getMonth();
+  const month = today.getMonth() + 1;
   const year = today.getFullYear();
-  console.log(day, month, year);
 
   useEffect(() => {
     axios
       .get(
-        `https://newsapi.org/v2/everything?q=world&language=en&sortBy=popularity&from=${}-${}-${}&apiKey=${API_KEY}`
+        `https://newsapi.org/v2/everything?q=world&language=en&sortBy=popularity&from=${year}-${
+          month < 10 ? `0${month}` : month
+        }-${day - 1}&apiKey=${API_KEY}`
       )
       .then((data) => {
         console.log(data.data.articles);
+        setTodaysArticles(data.data.articles);
       });
   }, []);
   // FETCH + USEEFFECT TO NIE JEST NAJLEPSZY SPOSÓB NA KOMUNIKACJE Z API W REACTCIE, ALTERNATYEWA: REACT QUERY, TEN SPOSÓB JEST MIMO WSZYSTKO OK
-  return <div>HomePage</div>;
+  const typographyStyles = { fontSize: "2rem", mt: ".8rem" };
+
+  return (
+    <>
+      <Typography variant="h2" align="center" sx={typographyStyles}>
+        Today's hottest news:
+      </Typography>
+      <List sx={{ width: "100%", alignContent: "center" }}></List>
+    </>
+  );
 };
 
 export default HomePage;

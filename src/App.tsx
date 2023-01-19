2a0ceb657aa3a564ac52e38c8a877b2b498b11d2
Kacper Żarna <kacper@192.168.1.3>
2023-01-19 19:22:01 +0100

    Kod 19.01.2023 19:20

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./components/HomePage/HomePage";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 import LoginPage from "./components/LoginPage/LoginPage";
+import ExcerciseForm from "./components/ExcerciseForm/ExcerciseForm";
 function App() {
   const [avatarClicked, setAvatarClicked] = useState<boolean>(false);
+  const [usersFirstName, setUsersFirstName] = useState<string>("")
+  console.log(usersFirstName)
   return (
     <div className="App">
       <BrowserRouter>
         {/* STATIC */}
         <Navbar setAvatarClicked={setAvatarClicked} />
+        <ExcerciseForm setUsersFirstName={setUsersFirstName}/>
         {/* STATIC */}
         {/* DYNAMIC */}
         <Routes>
@@ -29,9 +33,4 @@ function App() {
 
 export default App;
 
-// 3. JSX:
-// - wszystko ma być obwinięte w komponent ListItem (MUI)
-// - w środku ListItem wszystko ma być obwinięte komponentem Card (MUI), props komponentu Card: variant na outlined, w sx'ach margines dolny na 10px
-// - w środku Card wszystko obwinięte tagiem a (zwykły anchor z htmla), props: href na url z artykułu który zostaw przekazany propsem do Article.tsx, target na __blank, w style textDecoration none
-// - w środku <a> ma się znaleźć tag img (zwykły htmlowy) z src ustawionym na urlToImage z artykułu z propsów, alt ustawiony na title artykułu z propsów i w style width ustawione na 100%
-// - obok tagu img ma się znaleźć komponent ListItemText (z MUI) w sx'ach color black, mx 5%. W środku ListItemText wyświetl tytuł artykułu z propsów.
+

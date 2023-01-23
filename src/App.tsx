ecf5b7251616d0d08b32edf206c780d50693f067
Kacper Żarna <kacper@192.168.1.3>
2023-01-23 20:09:12 +0100

    Kod 23.01.2023 20:10

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import LoginPage from "./components/LoginPage/LoginPage";
 import ExcerciseForm from "./components/ExcerciseForm/ExcerciseForm";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./helpers/firebaseConfig";
-
+import UserPage from "./components/UserPage/UserPage";
+import { Typography } from "@mui/material";
 function App() {
   const [avatarClicked, setAvatarClicked] = useState<boolean>(false);
   const [usersFirstName, setUsersFirstName] = useState<string>("");
@@ -29,7 +30,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         {/* STATIC */}
-        <Navbar setAvatarClicked={setAvatarClicked} loggedIn={loggedIn}/>
+        <Navbar setAvatarClicked={setAvatarClicked} loggedIn={loggedIn} />
         {/* <ExcerciseForm setUsersFirstName={setUsersFirstName}/> */}
         {/* STATIC */}
         {/* DYNAMIC */}
@@ -39,6 +40,7 @@ function App() {
           <Route path="/home" element={<HomePage />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/user" element={<UserPage loggedIn={loggedIn} />} />
         </Routes>
         {/* DYNAMIC */}
         {/* STATIC */}
@@ -51,9 +53,3 @@ function App() {
 export default App;
 
 
-// 1. Stwórz komponent UserPage.
-// 2. Komponent UserPage wyświetlaj jako Route w App.tsx, od razu przekaż do UserPage stan loggedIn, nie zapomnij o interface w UserPage.
-// 3. W UserPage wyświetlaj react fragment, w środku:
-// - Typography variant h2, align center, w sxach: fontSize 2rem, my 1rem, borderBottom 1px solid #1976d2, pb .5rem. Text: Your profile
-// - Typography variant h5, align center, w sxach: fontSize 1rem, my 1rem, mx auto. Text: Your email: *tutaj email użytkownika*. Email użytkownika jest dostępny na obiekcie auth (zaimportuj go z firebaseConfig), auth.currentUser.email
-// - Button variant outlined, w sxach: display: block, mx auto, my 1rem. Nadaj mu onClick w którym będziesz wylogowywać użytkownika. Do tego posłuży ci funkcja signOut importowana z firebase/auth. Sama funkcja signOut przyjmuje 1 arugment: obiekt auth (firebaseConfig). Text buttona: Log out

8298b1ed04198b8b594b26d4900b1a54b9e931ac
AlexVoynov <voynov.alexandr@gmail.com>
2023-09-28 12:41:26 +0200

    changes in api.ts

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,2 +1,3 @@
 //export const API_KEY = "a63d3758448447309280a5650ac004e4";
-export const API_KEY = "e99ceb229a444147b35f5b62a693e173";
+//export const API_KEY = "e99ceb229a444147b35f5b62a693e173";
+export const API_KEY = "744d9905fe0a4e0591010d1db891dcfb";
